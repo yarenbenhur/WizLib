083b8927c5b68be4dbce4653e76c3bd73070e2f5
yarenbenhur <benhur19@itu.edu.tr>
2022-05-23 13:21:21 +0300

    CRUD - Upsert and Delete

---
+++ WizLib/Controllers/CategoryController.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WizLib_DataAccess.Data;
+using WizLib_Model.Models;
+
+namespace WizLib.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            List<Category> objList = _db.Categories.ToList();
+            return View(objList);
+        }
+        public IActionResult Upsert(int? id)
+        {
+            Category obj = new Category();
+            if (id == null)
+            {
+                return View(obj);
+            }
+            else
+            {
+                obj = _db.Categories.First(u => u.Category_Id == id);
+                return (obj == null) ? NotFound() : View(obj);
+            }
+           
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Upsert(Category obj)
+        {
+
+            if (ModelState.IsValid)
+            {
+                if (obj.Category_Id == 0 )
+                {
+                    _db.Categories.Add(obj);
+                }
+                else
+                {
+                    _db.Categories.Update(obj);
+                }
+                _db.SaveChanges();
+                return RedirectToAction(nameof(Index));
+            }
+
+            return View(obj);
+
+        }
+        public IActionResult Delete(int id)
+        {
+            var objFromDb = _db.Categories.FirstOrDefault(u => u.Category_Id == id);
+            _db.Categories.Remove(objFromDb);
+            _db.SaveChanges();
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
