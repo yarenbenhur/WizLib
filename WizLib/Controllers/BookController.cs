25c341111f167ee983ff8b53e1615c0bae9cee7b
yarenbenhur <benhur19@itu.edu.tr>
2022-05-24 13:38:08 +0300

    Book Detail View Controller

--- WizLib/Controllers/BookController.cs
+++ WizLib/Controllers/BookController.cs
@@ -18,15 +18,24 @@ namespace WizLib.Controllers
         {
             _db = db;
         }
+
         public IActionResult Index()
         {
             List<Book> objList = _db.Books.ToList();
+            foreach(var obj in objList)
+            {
+               /* obj.Publisher = _db.Publishers.FirstOrDefault(i => i.Publisher_Id == obj.Publisher_Id);*///loads number of books, least efficient
+                _db.Entry(obj).Reference(i => i.Publisher).Load(); // loads number of publisher
+            }
+           
             return View(objList);
         }
+
         public IActionResult Upsert(int? id)
         {
             BookVM obj = new BookVM();
             obj.PublisherList = _db.Publishers.Select(i => new SelectListItem { Text = i.Name, Value = i.Publisher_Id.ToString() });
+
             if (id == null)
             {
                 return View(obj);
@@ -37,8 +46,8 @@ namespace WizLib.Controllers
                 return NotFound();
             }
             return View(obj);
-
         }
+
         [HttpPost]
         [ValidateAntiForgeryToken]
         public IActionResult Upsert(BookVM obj)
@@ -55,6 +64,47 @@ namespace WizLib.Controllers
             _db.SaveChanges();
             return RedirectToAction(nameof(Index));
         }
+
+        public IActionResult Details(int? id)
+        {
+            BookVM obj = new BookVM();
+           
+            if (id == null)
+            {
+                return View(obj);
+            }
+            obj.Book = _db.Books.FirstOrDefault(U => U.Book_Id == id);
+            obj.Book.Detail = _db.Details.FirstOrDefault(u => u.Detail_Id == obj.Book.Detail_Id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+            return View(obj);
+
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Details(BookVM obj)
+        {
+
+            if (obj.Book.Detail.Detail_Id == 0)
+            {
+                _db.Books.Add(obj.Book);
+                _db.SaveChanges();
+                var BookFromDb = _db.Books.FirstOrDefault(u => u.Book_Id == obj.Book.Book_Id);
+                BookFromDb.Detail_Id = obj.Book.Detail_Id;
+                _db.SaveChanges();
+            }
+            else
+            {
+                _db.Details.Update(obj.Book.Detail);
+                _db.SaveChanges();
+            }
+           
+            return RedirectToAction(nameof(Index));
+        }
+
         public IActionResult Delete(int id)
         {
             var objFromDb = _db.Books.FirstOrDefault(u => u.Book_Id == id);
