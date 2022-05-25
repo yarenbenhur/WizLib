75eebda6f1bc6d35890244e1af64ee0ddd957a6a
yarenbenhur <benhur19@itu.edu.tr>
2022-05-25 14:34:55 +0300

    Eager Loading and Author View-Controller

--- WizLib/Controllers/BookController.cs
+++ WizLib/Controllers/BookController.cs
@@ -21,16 +21,19 @@ namespace WizLib.Controllers
 
         public IActionResult Index()
         {
-            List<Book> objList = _db.Books.ToList();
-            foreach(var obj in objList)
-            {
-               /* obj.Publisher = _db.Publishers.FirstOrDefault(i => i.Publisher_Id == obj.Publisher_Id);*///loads number of books, least efficient
-                _db.Entry(obj).Reference(i => i.Publisher).Load(); // loads number of publisher
-            }
+            List<Book> objList = _db.Books.Include(u=>u.Publisher).Include(u=>u.BookAuthors).ThenInclude(i => i.Author).ToList();
+          
+            //List<Book> objList = _db.Books.ToList();
+            //foreach (var obj in objList)
+            //{
+            //   /* obj.Publisher = _db.Publishers.FirstOrDefault(i => i.Publisher_Id == obj.Publisher_Id);*///loads number of books, least efficient
+            //    _db.Entry(obj).Reference(i => i.Publisher).Load(); // loads number of publisher
+            //}
            
             return View(objList);
         }
-
+        
+        [HttpGet]
         public IActionResult Upsert(int? id)
         {
             BookVM obj = new BookVM();
@@ -64,7 +67,8 @@ namespace WizLib.Controllers
             _db.SaveChanges();
             return RedirectToAction(nameof(Index));
         }
-
+        
+        [HttpGet]
         public IActionResult Details(int? id)
         {
             BookVM obj = new BookVM();
@@ -73,8 +77,8 @@ namespace WizLib.Controllers
             {
                 return View(obj);
             }
-            obj.Book = _db.Books.FirstOrDefault(U => U.Book_Id == id);
-            obj.Book.Detail = _db.Details.FirstOrDefault(u => u.Detail_Id == obj.Book.Detail_Id);
+            obj.Book = _db.Books.Include(u=>u.Detail).FirstOrDefault(U => U.Book_Id == id);
+    
             if (obj == null)
             {
                 return NotFound();
@@ -111,6 +115,39 @@ namespace WizLib.Controllers
             _db.Books.Remove(objFromDb);
             _db.SaveChanges();
             return RedirectToAction(nameof(Index));
+        }
+
+        [HttpGet]
+        public IActionResult ManageAuthors(int id)
+        {
+            BookAuthorVM obj = new BookAuthorVM
+            {
+                Book = _db.Books.FirstOrDefault(u => u.Book_Id == id),
+                BookAuthorList = _db.BookAuthors.Include(u=> u.Author).Include(u=> u.Book).Where(u => u.Book_Id == id).ToList(),
+                BookAuthor = new BookAuthor() { Book_Id = id }
+
+            };
+            var tempListOfAssignedAuthors = obj.BookAuthorList.Select(u => u.Author_Id).ToList();
+            var tempList = _db.Authors.Where(u => !tempListOfAssignedAuthors.Contains(u.Author_Id)).ToList();
+            obj.AuthorSelectList = tempList.Select(u => new SelectListItem
+            {
+                Text = u.FullName,
+                Value = u.Author_Id.ToString()
+            });
+            return View(obj);
+        }
+
+        [HttpPost]
+        public IActionResult ManageAuthors(BookAuthorVM bookAuthorVM)
+        {
+            if (bookAuthorVM.BookAuthor.Book_Id !=0 && bookAuthorVM.BookAuthor.Author_Id !=0)
+            {
+                _db.BookAuthors.Add(bookAuthorVM.BookAuthor);
+                _db.SaveChanges();
+
+            }
+            return RedirectToAction(nameof(ManageAuthors), new { @id = bookAuthorVM.BookAuthor.Book_Id });
+
         }
     }
 }
---
+++ WizLib_Model/ViewModel/BookAuthorVM.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WizLib_Model.Models;
+
+namespace WizLib_Model.ViewModel
+{
+    public class BookAuthorVM
+    {
+        public BookAuthor BookAuthor { get; set; }
+
+        public Book Book{ get; set; }
+        public IEnumerable<BookAuthor> BookAuthorList { get; set; }
+        public IEnumerable<SelectListItem> AuthorSelectList { get; set; }
+
+    }
+}
